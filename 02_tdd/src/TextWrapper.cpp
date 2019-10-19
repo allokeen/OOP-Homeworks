3cca088412a7c6b94a06d689176859ed07dd0e2a
Nikola <nikswieagh@gmail.com>
2019-10-19 17:17:39 +0200

    [green] 3nd test of WrappingTextWithSpaces: test passed - first space in line omitted with "continue"

--- 02_tdd/src/TextWrapper.cpp
+++ 02_tdd/src/TextWrapper.cpp
@@ -9,7 +9,8 @@ std::string TextWrapper::Wrapping(int columnsOut, std::string text) {
         std:: string bufor;
 
         for( int i=0; i<text.length(); i++)
-            {
+        {       if(i%columnsOut==0 && text[i]==' ')
+                    continue;
                 bufor = bufor + text[i];
                 if(i!=text.length()-1  && i%columnsOut==columnsOut-1)
                     bufor = bufor + "\n";

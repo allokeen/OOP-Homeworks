fec2186959bf96073deab8275159009e363b0d9b
Nikola <nikswieagh@gmail.com>
2019-10-19 14:42:50 +0200

    [red] TEST WrappingGivenText : method "wrapping" does not exist

--- 02_tdd/test/src/TextWrapperTest.cpp
+++ 02_tdd/test/src/TextWrapperTest.cpp
@@ -12,3 +12,9 @@ TEST(TextWrapper, HasColumnsGetter) {
 
     EXPECT_EQ(10, wrapper.columns());
 }
+
+TEST( TextWrapper, WrappingGivenText)
+{
+    auto wrapper = TextWrapper{};
+    EXPECT_EQ( "", wrapper.Wrapping(1,""));
+}

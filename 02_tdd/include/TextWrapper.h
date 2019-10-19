04aa3acf789da94226bbdc49aff48d82a1073205
Nikola <nikswieagh@gmail.com>
2019-10-19 14:48:39 +0200

    [green] TEST WrappingGivenText: method declared, test passed for ""

--- 02_tdd/include/TextWrapper.h
+++ 02_tdd/include/TextWrapper.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <string>
+
 class TextWrapper {
 public:
 
     int columns() const;
+
+    std::string Wrapping( int, std::string);
 };
--- 02_tdd/src/TextWrapper.cpp
+++ 02_tdd/src/TextWrapper.cpp
@@ -3,3 +3,7 @@
 int TextWrapper::columns() const {
     return 10;
 }
+
+std::string TextWrapper::Wrapping(int columnOut, std::string text) {
+    return "";
+}

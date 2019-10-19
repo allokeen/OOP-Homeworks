58fbfb3f0ff5489f6456a6f3904e270e23cbd20f
Nikola <nikswieagh@gmail.com>
2019-10-19 15:45:44 +0200

    [green] 3nd test of WrappingGivenText: adding conditions and loop in method, test passed

--- 02_tdd/src/TextWrapper.cpp
+++ 02_tdd/src/TextWrapper.cpp
@@ -4,6 +4,19 @@ int TextWrapper::columns() const {
     return 10;
 }
 
-std::string TextWrapper::Wrapping(int columnOut, std::string text) {
-    return text;
+std::string TextWrapper::Wrapping(int columnsOut, std::string text) {
+
+        std:: string bufor;
+        if( text.length() < 2)
+            return text;
+        else
+        {
+            for( int i=0; i<text.length(); i++)
+            {
+                bufor = bufor + text[i];
+                if(i!=text.length()-1)
+                    bufor = bufor + "\n";
+            }
+            return bufor;
+        }
 }
